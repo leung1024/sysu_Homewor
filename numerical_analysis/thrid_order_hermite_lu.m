cd3228cc9284c2b2db58e2325788319d4431549c
Arron <cljun8@hotmail.com>
2017-12-13 20:23:15 +0800

    update thrid_order_hermite_lu

--- numerical_analysis/thrid_order_hermite_lu.m
+++ numerical_analysis/thrid_order_hermite_lu.m
@@ -1,23 +1,16 @@
-function f = thrid_order_hermite_lu(X, XX, YY, Yd)
+function h_3 = thrid_order_hermite_lu(X, XX, YY, Yd)
     x_len = length(X);
-    interp_point_num = length(XX)
-    f = zeros(1, x_len);
-    a_1 = zeros(x_len)
+    interp_point_num = length(XX);
+    h_3 = zeros(1, x_len);
+    a_1 = zeros(interp_point_num, x_len);
+    a_2 = zeros(interp_point_num, x_len);
+    b_1 = zeros(interp_point_num, x_len);
+    b_2 = zeros(interp_point_num, x_len);
     for i = 1:(interp_point_num-1),
-        a_1(i) = (1 .+ 2.*((X - XX(i)) ./ (XX(i+1) - XX(i)))) .* ((X - XX(i+1)) ./ (XX(i) - XX(i+1))).^2;
-        a_2(i) = (1 .+ 2.*((X - XX(i+1)) ./ (XX(i+1) - XX(i)))) .* ((X - XX(i)) ./ (XX(i+1) - XX(i))).^2;
-        b_1(i) = (X - XX(i)) .* ((X - XX(i+1)) ./ (XX(i) - XX(i+1))).^2;
-        b_2(i) = (X - XX(i+1)) .* ((X - XX(i)) ./ (XX(i+1) - XX(i))).^2;
-    for i=1:interp_point_num
-        h = 1.0;
-        a = 0.0;
-        for j=1:interp_point_num
-            if( j ~= i)
-                h = h.*(X-XX(j)).^2./((XX(i)-XX(j)).^2);
-                a = a + 1/(XX(i)-XX(j));
-            end
-        end
-        f= f + h.*((XX(i)-X).*(2.*a.*YY(i)-Yd(i))+YY(i));
+        a_1(i,:) = (1 + 2.*((X - XX(i)) ./ (XX(i+1) - XX(i)))) .* ((X - XX(i+1)) ./ (XX(i) - XX(i+1))).^2;
+        a_2(i,:) = (1 + 2.*((X - XX(i+1)) ./ (XX(i) - XX(i+1)))) .* ((X - XX(i)) ./ (XX(i+1) - XX(i))).^2;
+        b_1(i,:) = (X - XX(i)) .* ((X - XX(i+1)) ./ (XX(i) - XX(i+1))).^2;
+        b_2(i,:) = (X - XX(i+1)) .* ((X - XX(i)) ./ (XX(i+1) - XX(i))).^2;
+        h_3 = h_3 + YY(i).*a_1(i,:) + YY(i+1).*a_2(i,:) + Yd(i).*b_1(i,:) + Yd(i+1).*b_2(i,:);
     end
-
 end

30fc5a6475a2f075acf8d15a8281ba4b232a78fb
Arron <cljun8@hotmail.com>
2017-12-13 16:37:56 +0800

    update liner_interp

--- numerical_analysis/liner_interp.m
+++ numerical_analysis/liner_interp.m
@@ -1,9 +1,9 @@
-function L_f = liner_interp(x, yy,a, b),
+function L_f = liner_interp(x, xx, yy),
     interp_point_num = length(yy);
-    seg_len = length(x) / interp_point_num
+    seg_len = floor(length(x) / (interp_point_num-1));
     L_f = zeros(1, length(x));
     for i = 1:(interp_point_num-1),
-        XX = [a a+1];
+        XX = [xx(i) xx(i+1)];
         YY = [yy(i) yy(i+1)];
         seg_start = (i-1)*seg_len + 1;
         seg_end = i*seg_len;

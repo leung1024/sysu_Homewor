d4d07a553780b6667a4a1cdb678376d484c4b5c3
Arron <cljun8@hotmail.com>
2018-01-03 21:45:02 +0800

    update

--- ML_course_new/final_homework_2/main.py
+++ ML_course_new/final_homework_2/main.py
@@ -1,10 +1,64 @@
 import numpy as np
+import matplotlib.pyplot as plt
 from neural_network import NeuralNetwork
 
-def main():
+N = 300
+
+def Exercise1_a():
     # create a 2-2-1 network
     network_size = np.array([2, 2, 1])
-    weights = np.array([[0.3, -0.4, -2],
-                        [-0.1, 1.0, 0.5]])
-    biases = np.array([[0.5, -0.5]])
-    net_obj = NeuralNetwork()
+    # set weights and bias
+    w_b = [np.array([[0.5, -0.5],
+                     [0.3, -0.4],
+                     [-0.1, 1]]),
+           np.array([[1],
+                     [-2],
+                     [0.5]])
+           ]
+    net_obj = NeuralNetwork([2,2,1], w_b)
+    # generate sample points
+    samples = np.random.uniform(-5, 5, [2, N])
+    samples[1,:] *= -1
+    plt.figure()
+    for index in range(samples.shape[1]):
+        x1 = samples[0, index]
+        x2 = samples[1, index]
+        rst = net_obj.feedforward(np.array([x1,x2]))
+        if rst <= 0:
+            plt.plot(x1, x2, 'or')
+        else:
+            plt.plot(x1, x2, 'ob')
+    plt.xlabel("X1")
+    plt.ylabel("X2")
+    plt.show()
+
+def Exercise1_b():
+    # create a 2-2-1 network
+    network_size = np.array([2, 2, 1])
+    # set weights and bias
+    w_b = [np.array([[-1, 1],
+                     [-0.5, 1.5],
+                     [1.5, -0.5]]),
+           np.array([[0.5],
+                     [-1],
+                     [1]])
+           ]
+    net_obj = NeuralNetwork([2,2,1], w_b)
+    # generate sample points
+    samples = np.random.uniform(-5, 5, [2, N])
+    samples[1,:] *= -1
+    plt.figure()
+    for index in range(samples.shape[1]):
+        x1 = samples[0, index]
+        x2 = samples[1, index]
+        rst = net_obj.feedforward(np.array([x1,x2]))
+        if rst <= 0:
+            plt.plot(x1, x2, 'or')
+        else:
+            plt.plot(x1, x2, 'ob')
+    plt.xlabel("X1")
+    plt.ylabel("X2")
+    plt.show()
+
+# Exercise1_a()
+# Exercise1_b()
--- ML_course_new/final_homework_2/neural_network.py
+++ ML_course_new/final_homework_2/neural_network.py
@@ -19,28 +19,44 @@ class NeuralNetwork():
     def __init__(self, network_sizes, w_b=[]):
         self.sizes = network_sizes
         self.layer_sizes = len(network_sizes)
-        if weights and biases:
+        self.output = []
+        self.raw_output = []
+        if w_b:
             [self.format_check(item) for item in w_b]
             self.w_b = w_b
         else:
-            pass
+            self.w_b = np.array([])
+            for layer_index in range(self.layer_sizes - 1):
+                cur_layer_size = network_sizes[layer_index] + 1
+                post_layer_size = network_sizes[layer_index+1] + 1
+                self.w_b = np.append(self.w_b, 2*np.random.rand(cur_layer_size, post_layer_size) - 1)
             # self.weights = np.array([np.random.randn(y, x) for x, y in zip(network_sizes[:-1], sizes[1:])])
             # self.biases = np.array([np.random.randn(y, 1) for y in network_sizes[1:]])
 
     def format_check(self, *args):
         for val in args:
-            if type(val) != np.narray:
+            if type(val) != np.ndarray:
                 raise TypeError
 
-    def activation_function(self, *args):
-        return numpy.tanh()
+    def transfer_function(self, input_val):
+        a = 1.716
+        b = 2/3
+        input_val *= b
+        return a * np.tanh(input_val)
 
     def feedforward(self, input_val):
         self.format_check(input_val)
         for layer_args in self.w_b:
+            input_val = np.insert(input_val, 0, 1)
             input_val = np.dot(input_val.T, layer_args)
-        output_val = numpy.tanh(input_val)
+            self.raw_output.append(input_val)
+            input_val = self.transfer_function(input_val)
+            self.a_output.append(input_val)
+        output_val = input_val
         return output_val
+
+    # def stochastic_backpropagation(self, theta, eta, m):
+        
 
 
 
